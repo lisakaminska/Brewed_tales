bbc2ebd5cedc4d26157dc670be7c3191be852618
lizakaminska <yelyzaveta.kaminska.shi.2023@lpnu.ua>
2024-11-04 19:33:35 +0200

    Fixed main repository and views.py.Also added some new customers into our project

--- brewed_tales/cafe/main.py
+++ brewed_tales/cafe/main.py
@@ -10,7 +10,7 @@ from cafe.repositories.BrewerContext import BrewerContext
 def display_customer_orders(customer_id):
     repo_facade = BrewerContext()
 
-    customer = repo_facade.get_customer_by_id(customer_id)
+    customer = repo_facade.customer_repo.get_customer_by_id(customer_id)
     if not customer:
         print(f"Customer with ID {customer_id} not found.")
         return
@@ -21,12 +21,12 @@ def display_customer_orders(customer_id):
     print(f"Email: {customer.email}")
 
     # Отримання всіх замовлень клієнта
-    orders = repo_facade.get_all_orders().filter(customer=customer)
+    orders = repo_facade.order_repo.get_all_orders().filter(customer=customer)
     if orders.exists():
         print(f"\nOrders for customer {customer.first_name} {customer.last_name}:")
         for order in orders:
             print(f"\nOrder ID: {order.id}, Date: {order.order_date}, Total: {order.total}")
-            order_items = repo_facade.get_all_order_items().filter(order=order)
+            order_items = repo_facade.order_item_repo.get_all_order_items().filter(order=order)
             for item in order_items:
                 item_description = f"{item.book.title if item.book else item.cafe_item.item_name}"
                 print(f" - {item_description} (Quantity: {item.quantity}, Price per unit: {item.price})")
@@ -37,33 +37,33 @@ def display_customer_orders(customer_id):
 def main():
     repo_facade = BrewerContext()
 
-    new_customer = repo_facade.add_customer('Oscar', 'Piastry', 23, 'osc@gmail.com')
+    new_customer = repo_facade.customer_repo.add_customer('Yurii', 'Kaminskyi', 28, 'yura@gmail.com')
     print(f"New customer: {new_customer.first_name} {new_customer.last_name}")
 
-    new_book = repo_facade.add_book(
-        title='The Little Prince',
-        author='Antonie de Saint-Exupery',
-        genre='Fantasy',
-        price=230.80,
-        publish_date='1965-09-07',
-        stock=170
+    new_book = repo_facade.book_repo.add_book(
+        title='The Thursday Murder Club',
+        author='Richard Osman',
+        genre='Criminal',
+        price=12.50,
+        publish_date='1997-09-16',
+        stock=100
     )
     print(f"New book: {new_book.title}")
 
-    new_cafe_item = repo_facade.add_item(
-        item_name='Tea',
-        item_description='Hot and cozy cup of tea',
-        price=55.90,
-        stock=300
+    new_cafe_item = repo_facade.cafe_item_repo.add_item(
+        item_name='Green Tea',
+        item_description='Freshly brewed green tea',
+        price=2.75,
+        stock=60
     )
     print(f"New cafe item: {new_cafe_item.item_name}")
 
     # Створення нового замовлення для клієнта
-    new_order = repo_facade.add_order(new_customer, order_items=[])
+    new_order = repo_facade.order_repo.add_order(new_customer, order_items=[])
 
     # Додавання елементів до замовлення
-    new_order_item_1 = repo_facade.add_order_item(new_order, book=new_book, quantity=1, price=new_book.price)
-    new_order_item_2 = repo_facade.add_order_item(new_order, cafe_item=new_cafe_item, quantity=2, price=new_cafe_item.price)
+    new_order_item_1 = repo_facade.order_item_repo.add_order_item(new_order, book=new_book, quantity=1, price=new_book.price)
+    new_order_item_2 = repo_facade.order_item_repo.add_order_item(new_order, cafe_item=new_cafe_item, quantity=1, price=new_cafe_item.price)
 
     # Розрахунок загальної вартості замовлення
     total = (new_order_item_1.quantity * new_order_item_1.price) + (new_order_item_2.quantity * new_order_item_2.price)
@@ -74,19 +74,19 @@ def main():
 
     # Відображення всіх книг
     print("\nList of all books:")
-    books = repo_facade.get_all_books()
+    books = repo_facade.book_repo.get_all_books()
     for book in books:
         print(f"{book.title} - Author: {book.author}, Price: {book.price}")
 
     # Відображення всіх клієнтів
     print("\nList of all customers:")
-    customers = repo_facade.get_all_customers()
+    customers = repo_facade.customer_repo.get_all_customers()
     for customer in customers:
         print(f"{customer.first_name} {customer.last_name} - Email: {customer.email}")
 
     # Відображення всіх замовлень
     print("\nList of all orders:")
-    orders = repo_facade.get_all_orders()
+    orders = repo_facade.order_repo.get_all_orders()
     for order in orders:
         print(f"Order {order.id} from {order.customer.first_name} {order.customer.last_name}, Total: {order.total}")
 
--- brewed_tales/cafe/views.py
+++ brewed_tales/cafe/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from .models import Book, CafeItem, Customer, Order, OrderItem
 from .serializer import BookSerializer, CafeItemSerializer, CustomerSerializer, OrderSerializer, OrderItemSerializer
 from .repositories.BrewerContext import BrewerContext
 
@@ -9,16 +8,16 @@ brewer_context = BrewerContext()
 
 class BookViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
-    queryset = brewer_context.get_all_books()
+    queryset = brewer_context.book_repo.get_all_books()
     serializer_class = BookSerializer
 
     def list(self, request):
-        books = brewer_context.get_all_books()
+        books = brewer_context.book_repo.get_all_books()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        book = brewer_context.get_book_by_id(pk)
+        book = brewer_context.book_repo.get_book_by_id(pk)
         if book is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = BookSerializer(book)
@@ -27,39 +26,39 @@ class BookViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
-            brewer_context.add_book(**serializer.validated_data)
+            brewer_context.book_repo.add_book(**serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        book = brewer_context.get_book_by_id(pk)
+        book = brewer_context.book_repo.get_book_by_id(pk)
         if book is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
-            brewer_context.update_book(pk, **serializer.validated_data)
+            brewer_context.book_repo.update_book(pk, **serializer.validated_data)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        book = brewer_context.get_book_by_id(pk)
+        book = brewer_context.book_repo.get_book_by_id(pk)
         if book is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        brewer_context.delete_book(pk)
+        brewer_context.book_repo.delete_book(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CafeItemViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
-    queryset = brewer_context.get_all_items()
+    queryset = brewer_context.cafe_item_repo.get_all_items()
     serializer_class = CafeItemSerializer
 
     def list(self, request):
-        items = brewer_context.get_all_items()
+        items = brewer_context.cafe_item_repo.get_all_items()
         serializer = CafeItemSerializer(items, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        item = brewer_context.get_item_by_id(pk)
+        item = brewer_context.cafe_item_repo.get_item_by_id(pk)
         if item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CafeItemSerializer(item)
@@ -68,39 +67,39 @@ class CafeItemViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = CafeItemSerializer(data=request.data)
         if serializer.is_valid():
-            brewer_context.add_item(**serializer.validated_data)
+            brewer_context.cafe_item_repo.add_item(**serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        item = brewer_context.get_item_by_id(pk)
+        item = brewer_context.cafe_item_repo.get_item_by_id(pk)
         if item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CafeItemSerializer(item, data=request.data)
         if serializer.is_valid():
-            brewer_context.update_item(pk, **serializer.validated_data)
+            brewer_context.cafe_item_repo.update_item(pk, **serializer.validated_data)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        item = brewer_context.get_item_by_id(pk)
+        item = brewer_context.cafe_item_repo.get_item_by_id(pk)
         if item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        brewer_context.delete_item(pk)
+        brewer_context.cafe_item_repo.delete_item(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CustomerViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    queryset = brewer_context.get_all_customers()
+    queryset = brewer_context.customer_repo.get_all_customers()
     serializer_class = CustomerSerializer
 
     def list(self, request):
-        customers = brewer_context.get_all_customers()
+        customers = brewer_context.customer_repo.get_all_customers()
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        customer = brewer_context.get_customer_by_id(pk)
+        customer = brewer_context.customer_repo.get_customer_by_id(pk)
         if customer is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CustomerSerializer(customer)
@@ -109,39 +108,39 @@ class CustomerViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
-            brewer_context.add_customer(**serializer.validated_data)
+            brewer_context.customer_repo.add_customer(**serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        customer = brewer_context.get_customer_by_id(pk)
+        customer = brewer_context.customer_repo.get_customer_by_id(pk)
         if customer is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = CustomerSerializer(customer, data=request.data)
         if serializer.is_valid():
-            brewer_context.update_customer(pk, **serializer.validated_data)
+            brewer_context.customer_repo.update_customer(pk, **serializer.validated_data)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        customer = brewer_context.get_customer_by_id(pk)
+        customer = brewer_context.customer_repo.get_customer_by_id(pk)
         if customer is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        brewer_context.delete_customer(pk)
+        brewer_context.customer_repo.delete_customer(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class OrderViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    queryset = brewer_context.get_all_orders()
+    queryset = brewer_context.order_repo.get_all_orders()
     serializer_class = OrderSerializer
 
     def list(self, request):
-        orders = brewer_context.get_all_orders()
+        orders = brewer_context.order_repo.get_all_orders()
         serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        order = brewer_context.get_order_by_id(pk)
+        order = brewer_context.order_repo.get_order_by_id(pk)
         if order is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = OrderSerializer(order)
@@ -150,39 +149,39 @@ class OrderViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = OrderSerializer(data=request.data)
         if serializer.is_valid():
-            brewer_context.add_order(**serializer.validated_data)
+            brewer_context.order_repo.add_order(**serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        order = brewer_context.get_order_by_id(pk)
+        order = brewer_context.order_repo.get_order_by_id(pk)
         if order is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = OrderSerializer(order, data=request.data)
         if serializer.is_valid():
-            brewer_context.update_order(pk, **serializer.validated_data)
+            brewer_context.order_repo.update_order(pk, **serializer.validated_data)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        order = brewer_context.get_order_by_id(pk)
+        order = brewer_context.order_repo.get_order_by_id(pk)
         if order is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        brewer_context.delete_order(pk)
+        brewer_context.order_repo.delete_order(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    queryset = brewer_context.get_all_order_items()
+    queryset = brewer_context.order_item_repo.get_all_order_items()
     serializer_class = OrderItemSerializer
 
     def list(self, request):
-        order_items = brewer_context.get_all_order_items()
+        order_items = brewer_context.order_item_repo.get_all_order_items()
         serializer = OrderItemSerializer(order_items, many=True)
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
-        order_item = brewer_context.get_order_item_by_id(pk)
+        order_item = brewer_context.order_item_repo.get_order_item_by_id(pk)
         if order_item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = OrderItemSerializer(order_item)
@@ -191,23 +190,23 @@ class OrderItemViewSet(viewsets.ModelViewSet):
     def create(self, request):
         serializer = OrderItemSerializer(data=request.data)
         if serializer.is_valid():
-            brewer_context.add_order_item(**serializer.validated_data)
+            brewer_context.order_item_repo.add_order_item(**serializer.validated_data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, pk=None):
-        order_item = brewer_context.get_order_item_by_id(pk)
+        order_item = brewer_context.order_item_repo.get_order_item_by_id(pk)
         if order_item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = OrderItemSerializer(order_item, data=request.data)
         if serializer.is_valid():
-            brewer_context.update_order_item(pk, **serializer.validated_data)
+            brewer_context.order_item_repo.update_order_item(pk, **serializer.validated_data)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        order_item = brewer_context.get_order_item_by_id(pk)
+        order_item = brewer_context.order_item_repo.get_order_item_by_id(pk)
         if order_item is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        brewer_context.delete_order_item(pk)
+        brewer_context.order_item_repo.delete_order_item(pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
