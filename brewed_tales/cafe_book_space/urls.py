3ae8cab31b715af905df0049dc998bda84c01a2d
sofiiaspodaryk <sofiia.spodaryk.shi.2023@lpnu.ua>
2024-11-05 20:37:42 +0200

    changed everything apart from order list and cafe items list

--- brewed_tales/cafe_book_space/urls.py
+++ brewed_tales/cafe_book_space/urls.py
@@ -7,8 +7,15 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('books/', views.book_list, name='book_list'),
     path('books/<int:pk>/', views.book_detail, name='book_detail'),
-    path('books/add/', views.add_book, name='add_book'),  # Змініть тут на add_book
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.update_book, name='update_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+
+    path('cafe_items/', views.cafe_items_list, name='cafe_items_list'),
+    path('cafe_items/add/', views.add_cafe_items, name='add_cafe_items'),
+    path('cafe_items/<int:pk>/delete/', views.delete_cafe_items, name='delete_cafe_items'),
+
+    path ('orders/ ', views.order_list, name='order_list'),
+    path ('orders/<int:pk>/ ', views.order_detail, name='order_detail'),
+
 ]
--- brewed_tales/cafe_book_space/views.py
+++ brewed_tales/cafe_book_space/views.py
@@ -2,8 +2,11 @@ import requests
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import BookForm
+from .forms import CafeItemForm
 from django.shortcuts import render, redirect, get_object_or_404
 from cafe.models import Book
+from cafe.models import CafeItem
+from cafe.models import Order
 
 API_URL = 'http://localhost:8000/api/books/'
 
@@ -58,3 +61,44 @@ def book_confirm_delete(request, pk):
         book.delete()
         return redirect('cafe_book_space:book_list')
     return render(request, 'cafe_book_space/book_confirm_delete.html', {'book': book})
+
+
+def cafe_items_list(request):
+    response = requests.get(API_URL)
+    cafe_items = response.json()
+    return render(request, 'cafe_book_space/cafe_items_list.html', {'cafe_items': cafe_items})
+
+def add_cafe_items(request):
+    if request.method == 'POST':
+        form = CafeItemForm(request.POST)
+        if form.is_valid():
+            response = requests.post(API_URL, data=form.cleaned_data)
+            if response.status_code == 201:
+                return redirect(reverse('cafe_book_space:cafe_items_list'))
+    else:
+        form = CafeItemForm()
+    return render(request, 'cafe_book_space/cafe_items_form.html', {'form': form})
+
+def delete_cafe_items(request, pk):
+    if request.method == 'POST':
+        response = requests.delete(f'{API_URL}{pk}/')
+        if response.status_code == 204:
+            return redirect(reverse('cafe_book_space:cafe_items_list'))
+    return render(request, 'cafe_book_space/cafe_items_confirm_delete.html', {'pk': pk})
+
+def cafe_items_confirm_delete(request, pk):
+    cafe_items = get_object_or_404(CafeItem, pk=pk)
+    if request.method == 'POST':
+        cafe_items.delete()
+        return redirect('cafe_book_space:cafe_items_list')
+    return render(request, 'cafe_book_space/cafe_items_confirm_delete.html', {'cafe_items': cafe_items})
+
+def order_list(request):
+    response = requests.get(API_URL)
+    order = response.json()
+    return render(request, 'cafe_book_space/order_list.html')
+
+def order_detail(request):
+    response = requests.get(API_URL)
+    order = response.json()
+    return render(request, 'cafe_book_space/order_detail.html')
